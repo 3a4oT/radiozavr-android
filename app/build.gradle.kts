16a693f189e5b077a9144ec9e19d652b97a334fd
Petro Rovenskyi <petro.rovenskyy@gmail.com>
2025-01-17 13:59:39 +0200

    set app version to 0.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,10 +11,10 @@ android {
 
     defaultConfig {
         applicationId = "com.rovenskyi.radio_lux_fm_lviv_streamer"
-        minSdk = 28
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

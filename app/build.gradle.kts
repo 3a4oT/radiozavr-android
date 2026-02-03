074ed81adfc2f42d0ac1af924c6fbe6282e941b2
Petro Rovenskyi <petro.rovenskyy@gmail.com>
2026-02-03 12:55:26 +0200

    chore(app): bump version to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.rovenskyi.radio_lux_fm_lviv_streamer"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.2.2"
+        versionCode = 5
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
